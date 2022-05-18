fc78036fd7fe458a71513c36792b7dcd80f909dd
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-05-18 12:25:31 -0600

    created three tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,11 +13,43 @@ RSpec.describe Board do
     expect(board.cells).to include("D4")
   end
 
-  it 'valid_coordinate checks if coordinates exist in cells' do
+   it 'valid_coordinate? checks if coordinates exist in cells' do
     board = Board.new
     board.create_cells
 
     expect(board.valid_coordinate?("A1")).to be(true)
     expect(board.valid_coordinate?("A5")).to be(false)
+  end
+
+  it 'valid_placement IDs incorrect placements based on ship size' do
+    board = Board.new
+    board.create_cells
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be(false)
+  end
+
+  it 'valid_placement IDs incorrect placements based on non consecutive coordinates' do
+    board = Board.new
+    board.create_cells
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to be(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be(false)
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be(false)
+  end
+
+  it 'valid_placement IDs correct placement' do
+    board = Board.new
+    board.create_cells
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to be(true)
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be(true)
   end
 end
