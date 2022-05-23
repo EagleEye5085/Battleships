266212a2827bfd9a8c9c274342d9553d9f51d71e
Thomas Turner <thomasturner482@yahoo.com>
2022-05-23 12:10:17 -0500

    custom board complete

--- lib/board.rb
+++ lib/board.rb
@@ -2,17 +2,20 @@ require './lib/cells'
 
 class Board
 
-  attr_reader :cells
+  attr_reader :cells, :range1, :range2
 
-  def initialize
+  def initialize(range1, range2)
     @cells = {}
+    @range1 = 1..range1
+    @range2 = "A"..range2
+    # require 'pry'; binding.pry
   end
 
  def create_cells
    # puts "please choose length of grid 4-25"
    # length = gets.chomp.to_i
-   @range1 = 1..4 #length
-   @range2 = "A".."D"
+   # @range1 = 1..4 #length
+   # @range2 = "A".."D"
    @range2.each do |letter|
      @range1.each do |number|
        @cells["#{letter}#{number}"] = Cell.new("#{letter}#{number}")
@@ -53,10 +56,25 @@ class Board
   end
 
   def render(bool = nil)
-    "  1 2 3 4 \n" +
-    "A #{cells["A1"].render(bool)} #{cells["A2"].render(bool)} #{cells["A3"].render(bool)} #{cells["A4"].render(bool)} \n" +
-    "B #{cells["B1"].render(bool)} #{cells["B2"].render(bool)} #{cells["B3"].render(bool)} #{cells["B4"].render(bool)} \n" +
-    "C #{cells["C1"].render(bool)} #{cells["C2"].render(bool)} #{cells["C3"].render(bool)} #{cells["C4"].render(bool)} \n" +
-    "D #{cells["D1"].render(bool)} #{cells["D2"].render(bool)} #{cells["D3"].render(bool)} #{cells["D4"].render(bool)} \n"
+    spacing = "   "
+    @range1.each do |number|
+      spacing += number.to_s + " "
+    end
+
+    @range2.each do |letter|
+      spacing += "\n#{letter} "
+      @range1.each do |number|
+        spacing += " #{cells["#{letter}#{number}"].render(bool)}"
+      end
+    end
+    spacing + "\n"
+
+    # puts
+    # "  1 2 3 4 \n" +
+    # "A #{cells["A1"].render(bool)} #{cells["A2"].render(bool)} #{cells["A3"].render(bool)} #{cells["A4"].render(bool)} \n" +
+    # "B #{cells["B1"].render(bool)} #{cells["B2"].render(bool)} #{cells["B3"].render(bool)} #{cells["B4"].render(bool)} \n" +
+    # "C #{cells["C1"].render(bool)} #{cells["C2"].render(bool)} #{cells["C3"].render(bool)} #{cells["C4"].render(bool)} \n" +
+    # "D #{cells["D1"].render(bool)} #{cells["D2"].render(bool)} #{cells["D3"].render(bool)} #{cells["D4"].render(bool)} \n"
   end
+  # require 'pry'; binding.pry
 end
